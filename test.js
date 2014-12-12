95238d3d865d31269e09f040d0ecc6184f978b6f
Dampe <isdampe@gmail.com>
2014-12-12 13:19:07 +0000

    It reverses the polarity, woot!

--- test.js
+++ test.js
@@ -7,20 +7,54 @@ pins.init(function(){
 
 	//Run motor for one second.
 	setTimeout(function(){
-		pins.writeToRelay("relay1", 0);
+		pins.writeToRelay("relay1", 1);
 
-		//Stop motor after one second.
 		setTimeout(function(){
 			pins.writeToRelay("relay2", 0);
 
-			//Swap polarity and direction of motor.
 			setTimeout(function(){
 				pins.writeToRelay("relay2", 1);
 
-			},1000);
 
 
-		},1000);		
+
+
+
+
+
+
+
+
+
+
+
+			setTimeout(function(){
+					pins.writeToRelay("relay1", 0);
+
+					setTimeout(function(){
+						pins.writeToRelay("relay2", 0);
+
+						setTimeout(function(){
+							pins.writeToRelay("relay2", 1);
+						}, 1000);
+
+					}, 1000);
+
+				},1000);
+
+
+
+
+
+
+
+
+
+
+			}, 1000);
+
+		}, 1000);
+
 	},1000);
 
 	/*
